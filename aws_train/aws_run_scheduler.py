edaae8cc351c44c9dab4dd061a31ef0424674e94
Aaron Kollasch <aaron@kollasch.dev>
2023-05-11 03:05:55 -0400

    Don't remove protein index if it is None

--- aws_train/aws_run_scheduler.py
+++ aws_train/aws_run_scheduler.py
@@ -321,7 +321,8 @@ if __name__ == "__main__":
         with open(args.database_path) as f:
             worker_database = json.load(f)
         for worker in worker_database["workers"].values():
-            protein_indices.remove(worker["current_index"])
+            if worker["current_index"] is not None:
+                protein_indices.remove(worker["current_index"])
     else:
         worker_database = {
             "finished_indices": [],
